34d05e13e0b506257b3f4e66cb5c291dd31c0f26
roronoazor <ugoodumegwu@gmail.com>
2024-08-06 05:54:52 +0100

    feat: removed routes from users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,15 +6,4 @@ import { User } from '@prisma/client';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  async create(
-    @Body() userData: { username: string; email: string; password: string },
-  ): Promise<User> {
-    return this.usersService.create(userData);
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User | null> {
-    return this.usersService.findById(id);
-  }
 }
